37d7b17a43b97b3df5b1974d581a49ac3a2a9e28
middleprince <hellangel.ah@gmail.com>
2019-08-01 23:22:58 +0800

    feat: Finsh algo to find item in ordered 2D matrix

---
+++ Basic-Algo/04_NumberInOrderedArray.cpp
@@ -0,0 +1,129 @@
+// 面试题4：二维数组中的查找
+// 题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按
+// 照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个
+// 整数，判断数组中是否含有该整数。
+
+#include <cstdio>
+
+bool findOneInArray(int* matrix, int rows, int columns, int item) {
+    bool found = false;
+    
+    if (matrix != nullptr && rows > 0 && columns > 0) {
+        int row_indx = 0; 
+        int col_indx = columns - 1;
+        
+        while (row_indx < rows && col_indx >= 0) {
+            // locate the item from top right of the matrix
+            int index_value = matrix[row_indx * columns + col_indx];
+            if (index_value == item) {
+                found = true;
+                break;
+            } else if (index_value < item)
+                ++row_indx;
+            else
+                --col_indx;
+        }
+    }
+    return found;
+}
+
+//==============================================================================
+//  Test function
+//==============================================================================
+
+void Test(char* testName, int* matrix, int rows, int columns, int number, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    bool result = findOneInArray(matrix, rows, columns, number);
+    if(result == expected)
+        printf("Passed.\n");
+    else
+        printf("Failed.\n");
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数在数组中
+void Test1()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test1", (int*)matrix, 4, 4, 7, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数不在数组中
+void Test2()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test2", (int*)matrix, 4, 4, 5, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最小的数字
+void Test3()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test3", (int*)matrix, 4, 4, 1, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最大的数字
+void Test4()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test4", (int*)matrix, 4, 4, 15, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最小的数字还小
+void Test5()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test5", (int*)matrix, 4, 4, 0, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最大的数字还大
+void Test6()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test6", (int*)matrix, 4, 4, 16, false);
+}
+
+// 鲁棒性测试，输入空指针
+void Test7()
+{
+    Test("Test7", nullptr, 0, 0, 16, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
